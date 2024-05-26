2260fcc59676af87c8bcd61555c2ff85f6937b2a
João Lobo <30907944+joaodiaslobo@users.noreply.github.com>
2024-05-26 12:30:36 +0100

    feat: detective users, case views and indexes (#8)

--- indices.sql
+++ indices.sql
@@ -7,35 +7,66 @@
 use temis_db;
 
 --
+-- Criação de índice (INDEX) sobre o atributo 'DataInício' da tabela 'Caso'.
+CREATE INDEX idxCasoDataInício ON Caso (DataInício);
+
+--
+-- Criação de índice (INDEX) sobre o atributo 'Estado' da tabela 'Caso'.
+CREATE INDEX idxCasoEstado ON Caso (Estado);
+
+--
 -- Criação de índice (INDEX) sobre o atributo 'nome' da tabela 'Cliente'.
-CREATE INDEX idx_cliente_nome ON Cliente (Nome);
+CREATE INDEX idxClienteNome ON Cliente (Nome);
 
 --
 -- Criação de índice (INDEX) sobre o atributo 'Cliente' da tabela 'ClienteTelefones'.
-CREATE INDEX idx_cliente_telefones_cliente ON ClienteTelefones (Cliente);
+CREATE INDEX idxClienteTelefonesCliente ON ClienteTelefones (Cliente);
 
 --
 -- Criação de índice (INDEX) sobre o atributo 'Cliente' da tabela 'ClienteEmails'.
-CREATE INDEX idx_cliente_emails_cliente ON ClienteEmails (Cliente);
+CREATE INDEX idxClienteEmailsCliente ON ClienteEmails (Cliente);
+
+--
+-- Criação de índice (INDEX) sobre o atributo 'nome' da tabela 'Detetive'.
+CREATE INDEX idxDetetiveNome ON Detetive (Nome);
+
+--
+-- Criação de índice (INDEX) sobre o atributo 'Detetive' da tabela 'DetetiveTelefones'.
+CREATE INDEX idxDetetiveTelefonesDetetive ON DetetiveTelefones (Detetive);
+
+--
+-- Criação de índice (INDEX) sobre o atributo 'Detetive' da tabela 'DetetiveEmails'.
+CREATE INDEX idxDetetiveEmailsDetetive ON DetetiveEmails (Detetive);
 
 
 -- Criação de índice (INDEX) sobre o atributo 'valor' da tabela 'Pagamento'.
-CREATE INDEX idx_pagamento_valor ON Pagamento (Valor);
+CREATE INDEX idxPagamentoValor ON Pagamento (Valor);
 
 --
 -- Visualização dos índices definidos.
+SHOW INDEX FROM Caso;
+
 SHOW INDEX FROM Cliente;
 SHOW INDEX FROM ClienteTelefones;
 SHOW INDEX FROM ClienteEmails;
+
+SHOW INDEX FROM Detetive;
+SHOW INDEX FROM DetetiveTelefones;
+SHOW INDEX FROM DetetiveEmails;
 
 SHOW INDEX FROM Pagamento;
 
 --
 -- Remoção de todos os índices criados anteriormente.
-DROP INDEX idx_cliente_nome ON Cliente;
-DROP INDEX idx_cliente_telefones_cliente ON ClienteTelefones;
-DROP INDEX idx_cliente_emails_cliente ON ClienteEmails;
-DROP INDEX idx_pagamento_valor ON Pagamento;
+DROP INDEX idxCasoDataInício ON Caso;
+DROP INDEX idxCasoEstado ON Caso;
+DROP INDEX idxClienteNome ON Cliente;
+DROP INDEX idxClienteTelefonesCliente ON ClienteTelefones;
+DROP INDEX idxClienteEmailsCliente ON ClienteEmails;
+DROP INDEX idxDetetiveNome ON Detetive;
+DROP INDEX idxDetetiveTelefonesDetetive ON DetetiveTelefones;
+DROP INDEX idxDetetiveEmailsDetetive ON DetetiveEmails;
+DROP INDEX idxPagamentoValor ON Pagamento;
 
 --
 -- Visualização de todos os índices criados sobre as tabelas da base de dados 'temis_db'.
